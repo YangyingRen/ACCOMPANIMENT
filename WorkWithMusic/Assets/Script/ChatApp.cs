ba5832ee13209b698fbf3f13e691059a8dec29a9
Yangying <ryysophie@126.com>
2021-10-02 02:04:45 -0400

    Update

--- WorkWithMusic/Assets/Script/ChatApp.cs
+++ WorkWithMusic/Assets/Script/ChatApp.cs
@@ -12,19 +12,31 @@ public class ChatApp : MonoBehaviour
     public float i=2;
     public float speed=0.1f;
     public AudioSource ReceiveMessage;
+    public string[] Bargain;
+    public string[] Talk;
+    public string[] Respond;
+    private int len;
+    private int r;
 
     // Start is called before the first frame update
     void Start()
     {
+    len=Bargain.Length;
+    
         
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   r=Random.Range(0,len);
         i-=speed*Time.deltaTime;
         if(i<=0){
-         SendMessageToChatAI("Test");
+            if(speed>=0.5f){
+        SendMessageToChatAI(Bargain[r]);
+  
+            }
+            else{
+         SendMessageToChatAI(Talk[r]);}
          speed+=0.1f;
          i=2;
          ReceiveMessage.Play();
@@ -68,10 +80,13 @@ public class ChatApp : MonoBehaviour
     }
 
     public void TestButton(){
-        SendMessageToChat("Test");
+        r=Random.Range(0,len);
+        SendMessageToChat(Respond[r]);
         Debug.Log("Button");
         speed=0.1f;
         i=2;
+        SendMessageToChatAI("Oh sorry, I don't mean to interrupt you.");
+
     }
 }
 [System.Serializable]
